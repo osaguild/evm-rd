b4178e123fa89c8d2f1dabf481deb86d6afc693a
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-01 22:17:57 +0900

    add Foo

--- scripts/deploy/all.ts
+++ scripts/deploy/all.ts
@@ -7,17 +7,17 @@ async function main() {
   const balance = (await deployer.getBalance()).toString()
   console.log('deploying contract with the account:', deployer.address)
   console.log('account balance:', balance)
-/*
+
   // token
   const tokenFactory = await ethers.getContractFactory('Token')
   const token = await tokenFactory.deploy()
   console.log('Token address:', token.address)
-*/
+
   // manager
   const managerFactory = await ethers.getContractFactory('Manager')
   const manager = await managerFactory.deploy(deployer.address)
   console.log('Manager address:', manager.address)
-/*
+
   // payable
   const payable1Factory = await ethers.getContractFactory('Payable1')
   const payable1 = await payable1Factory.deploy()
@@ -30,7 +30,11 @@ async function main() {
   const payable3Factory = await ethers.getContractFactory('Payable3')
   const payable3 = await payable3Factory.deploy()
   console.log('Payable3 address:', payable3.address)
-  */
+
+  // Foo
+  const fooFactory = await ethers.getContractFactory('Foo')
+  const foo = await fooFactory.deploy()
+  console.log('Foo address:', foo.address)
 }
 
 main()
