0cc9617dc4f1e71b56bb0d558faecaa66ecc8159
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-01 21:58:04 +0900

    add manager script via ethers

---
+++ scripts/contract/manager.ts
@@ -0,0 +1,8 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { ethers } from 'hardhat'
+import { Manager__factory } from '../../typechain-types'
+import { config } from '../config'
+;(async () => {
+  const [deployer] = await ethers.getSigners()
+  await Manager__factory.connect(config.MANAGER_CONTRACT, deployer as SignerWithAddress).setMessage('hello world')
+})()
