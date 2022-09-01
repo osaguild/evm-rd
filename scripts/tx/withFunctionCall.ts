022fee7abb17c23112872cda04f403fba2533d48
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-01 23:07:19 +0900

    fix tx

--- scripts/tx/withFunctionCall.ts
+++ scripts/tx/withFunctionCall.ts
@@ -1,7 +1,12 @@
 import { ethers } from 'ethers'
 import fetch from 'node-fetch'
 import { config } from '../config'
-import { convertFunctionToSelector, convertDynamicTypeToByteWithPadding } from '../utils/converter'
+import {
+  convertBoolToByteWithPadding,
+  convertDynamicTypeToByteWithPadding,
+  convertFunctionToSelector,
+  convertNumberToByteWithPadding,
+} from '../utils/converter'
 import 'dotenv/config'
 import { remove0x } from '../utils/util'
 
@@ -12,15 +17,25 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY as string
   const wallet = new ethers.Wallet(PRIVATE_KEY)
   console.log('wallet address:', wallet.address)
 
-  const { value, length, at } = convertDynamicTypeToByteWithPadding('hello world', 32)
+  const { value, length, at } = convertDynamicTypeToByteWithPadding('dave', 96)
   const tx = {
-    to: config.MANAGER_CONTRACT,
-    value: ethers.utils.parseEther('0.001'),
-    data: convertFunctionToSelector('setMessageWithEth(string)') + remove0x(at) + remove0x(length) + remove0x(value),
+    to: config.FOO_CONTRACT,
+    //    value: ethers.utils.parseEther('0.001'),
+    data:
+      convertFunctionToSelector('sam(bytes,bool,uint256[])') +
+      remove0x(at) +
+      remove0x(convertBoolToByteWithPadding(true)) +
+      remove0x(convertNumberToByteWithPadding(160)) +
+      remove0x(length) +
+      remove0x(value) +
+      remove0x(convertNumberToByteWithPadding(3)) +
+      remove0x(convertNumberToByteWithPadding(1)) +
+      remove0x(convertNumberToByteWithPadding(2)) +
+      remove0x(convertNumberToByteWithPadding(3)),
     gasLimit: '50000', // 21000 is the default, 53000 is contract creation
     maxPriorityFeePerGas: ethers.utils.parseUnits('5', 'gwei'), // for EIP1559
     maxFeePerGas: ethers.utils.parseUnits('20', 'gwei'), // for EIP1559
-    nonce: 53,
+    nonce: 59,
     type: 2, // 2 means EIP1559 transaction
     chainId: 5, // goerli
   }
