b9f79a84add1b18bd2f2cc92da90f66892c04f1e
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-19 08:47:52 +0900

    change dir

--- scripts/call/manager.ts
+++ scripts/call/manager.ts
@@ -1,7 +1,7 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { ethers } from 'hardhat'
 import { Manager__factory } from '../../typechain-types'
-import { config } from '../config'
+import { goerli as config } from '../config'
 ;(async () => {
   const [deployer] = await ethers.getSigners()
   await Manager__factory.connect(config.MANAGER_CONTRACT, deployer as SignerWithAddress).setMessage('hello world')
