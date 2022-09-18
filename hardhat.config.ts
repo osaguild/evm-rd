54eb9821d635cb31fc88766085ce14bed7810742
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-19 07:40:57 +0900

    add mumbai to network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,7 +10,9 @@ import * as dotenv from 'dotenv'
 const PATH_TO_HARDHAT_ENV = `${__dirname}/.env`
 dotenv.config({ path: PATH_TO_HARDHAT_ENV })
 
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY
+const ALCHEMY_RINKEBY_API_KEY = process.env.ALCHEMY_RINKEBY_API_KEY
+const ALCHEMY_GOERLI_API_KEY = process.env.ALCHEMY_GOERLI_API_KEY
+const ALCHEMY_MUMBAI_API_KEY = process.env.ALCHEMY_MUMBAI_API_KEY
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const COIN_MARKET_CAP_API_KEY = process.env.COIN_MARKET_CAP_API_KEY
@@ -19,11 +21,15 @@ module.exports = {
   solidity: '0.8.9',
   networks: {
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_GOERLI_API_KEY}`,
       accounts: [`${PRIVATE_KEY}`],
     },
     rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_RINKEBY_API_KEY}`,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_MUMBAI_API_KEY}`,
       accounts: [`${PRIVATE_KEY}`],
     },
   },
