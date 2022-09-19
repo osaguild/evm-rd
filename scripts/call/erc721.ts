2503a72e80c059666442720ca9b263365f8e442e
osaguild <nobuhisa.abe.0922@gmail.com>
2022-09-19 11:13:15 +0900

    add mint script for upgradeable

--- scripts/call/erc721.ts
+++ scripts/call/erc721.ts
@@ -1,8 +1,11 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { ethers } from 'hardhat'
-import { Erc721__factory } from '../../typechain-types'
+import { Erc721__factory, Erc721Upgradeable__factory } from '../../typechain-types'
 import { mumbai as config } from '../config'
 ;(async () => {
   const [deployer] = await ethers.getSigners()
   await Erc721__factory.connect(config.ERC721_CONTRACT, deployer as SignerWithAddress).mint(config.SUB1_WALLET)
+  await Erc721Upgradeable__factory.connect(config.ERC721UPGRADEABLE_CONTRACT, deployer as SignerWithAddress).mint(
+    config.SUB1_WALLET
+  )
 })()
