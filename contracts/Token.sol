72d42cbf7e33824167502765fb57af616f4fdd02
osaguild <nobuhisa.abe.0922@gmail.com>
2022-10-14 09:49:58 +0900

    fix solidity version

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity =0.8.9;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
